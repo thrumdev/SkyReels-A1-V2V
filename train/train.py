33510d3decee3988c74e8083f0535b66d95cf3b6
Robert Habermeier <rphmeier@gmail.com>
2025-07-17 17:53:03 -0500

    fix print

--- train/train.py
+++ train/train.py
@@ -326,7 +326,7 @@ class Trainer:
                     if self.accelerator.is_main_process:
                         timestamp = time.strftime("%H:%M:%S")
                         print(f"[{timestamp}] Step {step}/{max_steps}, Loss: {batch_loss:.4f}")
-                        print(f"\t inpaint loss={batch_inpaint_loss} optical loss={batch_optical_loss}")
+                        print(f"\t inpaint loss={log_dict['inpaint_loss']} optical loss={log_dict['optical_flow_loss']}")
                     if step % save_frequency == 0 and self.accelerator.is_main_process:
                         self.save(step)
 
